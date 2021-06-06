7a27757c07b581f4a9b50375672bc56da3beae2f
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-06-05 18:59:46 -0600

    Update kodiTV.sh

--- kodiTV.sh
+++ kodiTV.sh
@@ -4,8 +4,23 @@ apt-get install kodi && apt-get install kodi-pvr-argustv
 
 cd /opt
 
+echo "              .-'''-.                                             "
+echo "             '   _    \ _______                                   "
+echo "     .     /   /` '.   \\  ___ `'.   .--.       .----.     .----. "
+echo "   .'|    .   |     \  ' ' |--.\  \  |__|        \    \   /    /  "
+echo " .'  |    |   '      |  '| |    \  ' .--.     .|  '   '. /'   /   "
+echo "<    |    \    \     / / | |     |  '|  |   .' |_ |    |'    /    "
+echo " |   | ____`.   ` ..' /  | |     |  ||  | .'     ||    ||    |    "
+echo " |   | \ .'   '-...-'`   | |     ' .'|  |'--.  .-''.   `'   .'    "
+echo " |   |/  .               | |___.' /' |  |   |  |   \        /     "
+echo " |    /\  \             /_______.'/  |__|   |  |    \      /      "
+echo " |   |  \  \            \_______|/          |  '.'   '----'       "
+echo " '    \  \  \                               |   /                 "
+echo "'------'  '---'                             `'-'                  "
+echo " " the
+echo " Thank you for using kodiTV to access IPTV.  To get M#U IPTV channel access, copy link below"
 echo "https://iptv-org.github.io/iptv/index.m3u"
-echo "copy the above link into the kodi M3U url slot"
+echo "Paste it into Kodi"
 
 cd ~
 
